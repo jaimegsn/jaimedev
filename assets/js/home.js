08e7255d9d5e26c6f7e8a13a34e1a42cd8ede20b
jaimegsn <jaime.jgsn2@gmail.com>
2024-03-10 02:36:38 -0300

    First Commit

---
+++ assets/js/home.js
@@ -0,0 +1,9 @@
+import data from "../settings/home.json" assert { type: "json" };
+
+
+
+const elementHTML = document.getElementById("sidebar-profile-infos");
+const contentHTML = `
+
+`;
+elementHTML.innerHTML += contentHTML;
